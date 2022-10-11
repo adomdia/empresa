df66c2c3cd708e2d340367b71ac056b3acba1aec
antoniojaun37 <antoniojuan.dominguez@iesdonana.org>
2022-10-11 20:35:40 +0200

    Carga inicial

---
+++ empresa.sql
@@ -0,0 +1,18 @@
+
+DROP TABLE IF EXISTS departamentos CASCADE;
+
+CREATE TABLE departamentos ( 
+    id           bigserial    PRIMARY KEY
+  , codigo       numeric(2)   NOT NULL UNIQUE
+  , denominacion varchar(255) NOT NULL 
+);
+
+--- Datos de prueba (fixtures): ---
+
+
+INSERT INTO departamentos (codigo, denominacion)
+    VALUES  (10, 'Informática') 
+          , (20, 'Administrativo')
+          , (30, 'Prevención')
+          , (40, 'Laboratorio')
+          , (50, 'Automoción');
