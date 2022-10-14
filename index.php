c8c5d0afcaf763c91276bd918d16a469fa3e8f19
antoniojaun37 <antoniojuan.dominguez@iesdonana.org>
2022-10-14 18:43:07 +0200

    Mi resolucion

---
+++ index.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Departamentos</title>
+</head>
+
+<body>
+    <?php
+    $desde_codigo = (isset($_GET['desde_codigo'])) ? trim($_GET['desde_codigo']) : null;
+    $hasta_codigo = (isset($_GET['hasta_codigo'])) ? trim($_GET['hasta_codigo']) : null;
+    $denominacion = (isset($_GET['denominacion'])) ? trim($_GET['denominacion']) : null;
+    ?>
+    <div>
+        <form action="" method="get">
+            <fieldset>
+                <legend>Criterios de búsqueda</legend>
+                <p>
+                    <label>
+                        Desde código:
+                        <input type="text" name="desde_codigo" size="8" value="<?= $desde_codigo ?>">
+                    </label>
+                </p>
+                <p>
+                    <label>
+                        Hasta código:
+                        <input type="text" name="hasta_codigo" size="8" value="<?= $hasta_codigo ?>">
+                    </label>
+                </p>
+                <p>
+                    <label>
+                        Denominación:
+                        <input type="text" name="denominacion" size="8" value="<?= $denominacion ?>">
+                    </label>
+                </p>
+                <button type="submit">Buscar</button>
+            </fieldset>
+        </form>
+    </div>
+    <?php
+    if (!(isset($denominacion)) && !(isset($desde_codigo)) && !(isset($hasta_codigo))) {
+        $pdo = new PDO('pgsql:host=localhost;dbname=empresa', 'empresa', 'empresa');
+        $pdo->beginTransaction();
+        $sent = $pdo->query('SELECT COUNT(*) 
+                               FROM departamentos');
+        $total = $sent->fetchColumn();
+        $sent = $pdo->query('SELECT *
+                               FROM departamentos');
+    } else {
+        $pdo = new PDO('pgsql:host=localhost;dbname=empresa', 'empresa', 'empresa');
+        $pdo->beginTransaction();
+        $sent = $pdo->query('LOCK TABLE departamentos IN SHARE MODE');
+        $sent = $pdo->prepare('SELECT COUNT(*)
+                                 FROM departamentos
+                                WHERE codigo BETWEEN :desde_codigo AND :hasta_codigo AND :denominacion = denominacion');
+        $sent->execute([
+            ':desde_codigo' => $desde_codigo,
+            ':hasta_codigo' => $hasta_codigo,
+            ':denominacion' => $denominacion,
+
+        ]);
+        $total = $sent->fetchColumn();
+        $sent = $pdo->prepare('SELECT *
+                                 FROM departamentos
+                                WHERE codigo BETWEEN :desde_codigo AND :hasta_codigo AND :denominacion = denominacion
+                             ORDER BY codigo');
+        $sent->execute([
+            ':desde_codigo' => $desde_codigo,
+            ':hasta_codigo' => $hasta_codigo,
+            ':denominacion' => $denominacion,
+
+        ]);
+        $pdo->commit();
+    }
+    ?>
+    <br>
+    <div>
+        <table style="margin: auto" border="1">
+            <thead>
+                <th>Código</th>
+                <th>Denominación</th>
+            </thead>
+            <tbody>
+                <?php foreach ($sent as $fila) : ?>
+                    <tr>
+                        <td><?= $fila['codigo'] ?></td>
+                        <td><?= $fila['denominacion'] ?></td>
+                    </tr>
+                <?php endforeach ?>
+            </tbody>
+        </table>
+        <p>Número total de filas: <?= $total ?></p>
+    </div>
+</body>
+
+</html>
